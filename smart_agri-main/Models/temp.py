bb51f165536b7abe56f6f9a2f6c90f83a0555ab4
SRINIVAS-26-06 <srinureddy42115@gmail.com>
2025-02-16 18:41:11 +0530

    first commit

---
+++ smart_agri-main/Models/temp.py
@@ -0,0 +1,7 @@
+import sys
+import joblib
+import sklearn
+
+print(f"Python version: {sys.version}")
+print(f"Joblib version: {joblib.__version__}")
+print(f"Scikit-learn version: {sklearn.__version__}")
